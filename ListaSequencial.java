688b844fb54c7a60656a9bebaf79862a470fbb9b
Miguel Figueiredo <142344702+miguel-ffo@users.noreply.github.com>
2024-03-13 18:37:08 -0300

    Versão 2.0
    Adição de Switch/Case com tabela de ações para dinanimizar o uso das funções.

--- ListaSequencial.java
+++ ListaSequencial.java
@@ -56,94 +56,127 @@ public class ListaSequencial {
 		 System.out.println("A lista tem "+ lista_sequencial.tamanhoLista() + " elementos");
 		 	
 		 int pos = 0;
-		 
-		 while ( true ) {
-			 
-			 System.out.println("\nInserindo Contato ");
-			
-			 
-			 System.out.println("Nome: ");
-			 String nome = ler.next();
-			 
-			 System.out.println("Telefone: ");
-			 String telefone = ler.next();
-			 
-			 Contato novoContato = new Contato (nome,telefone);
-			 
-			 
-			 
-			 if (lista_sequencial.inserirContato(pos, novoContato)) {
-				 System.out.println("\nContato Salvo!");
+		 int opcao = 0;
+		 while (opcao != 5) {
+			  System.out.println("\nAções: \n\n1 - Inserir\n2 - Buscar\n3 - Remover\n4 - Mostrar Lista\n5 - Sair\n");
+		  	  opcao = ler.nextInt();
+		
+			 switch (opcao) {
+			 case 1:
+				 while ( true ) {
 				 
-			 }else {
-			 
-				 System.out.println("\n\nLista Cheia!!");
-				 break;
+				 System.out.println("\nInserindo Contato ");
+				
+				 
+				 System.out.println("Nome: ");
+				 String nome = ler.next();
+				 
+				 System.out.println("Telefone: ");
+				 String telefone = ler.next();
+				 
+				 Contato novoContato = new Contato (nome,telefone);
+				 
+				 
+				 
+				 if (lista_sequencial.inserirContato(pos, novoContato)) {
+					 System.out.println("\nContato Salvo!");
+					 
+				 }else {
+				 
+					 System.out.println("\n\nLista Cheia!!");
+					 break;
+				 }
+				 
+				 pos++;
+				 
+				 
+				
+				 
+				 
+				 System.out.println("(Continuar inserindo? (S/N)");
+				 String continuar = ler.next();
+				 
+				 if (continuar.equalsIgnoreCase("N")) {
+					 break;
+				 }
+				 
+				
+				 
 			 }
-			 
-			 pos++;
-			 System.out.println("\n	Lista: ");
-			 
-			 lista_sequencial.ordenarLista();
-			 lista_sequencial.exibirLista();
-			 
-			 
-			 System.out.println("(Continuar inserindo? (S/N)");
-			 String continuar = ler.next();
-			 
-			 if (continuar.equalsIgnoreCase("N")) {
 				 break;
-			 }
-			 
-			 
-			 
-		 }
-		 while ( true ) {
-			System.out.println("	Buscar Contatos:\n");
+			 case 2:
+				 
+				 while ( true ) {
+					 System.out.println("	Buscar Contatos:\n");
 			
-			 System.out.println("Nome: ");
-			 String contatobuscar = ler.next();
+					 System.out.println("Nome: ");
+					 String contatobuscar = ler.next();
 			 
-			 Contato contatoEncontrado = lista_sequencial.Buscar(contatobuscar);
+					 Contato contatoEncontrado = lista_sequencial.Buscar(contatobuscar);
 			 
-			 if (contatoEncontrado != null) {
-				 System.out.println("	Contato encontrado: \n\n	Nome: "+ contatoEncontrado.nome + "\n	Telefone: " + contatoEncontrado.telefone);
-			 }else {
-				 System.out.println("	Contato inexistente ou nome errado.");
-			 }
+					 if (contatoEncontrado != null) {
+						 System.out.println("	Contato encontrado: \n\n	Nome: "+ contatoEncontrado.nome + "\n	Telefone: " + contatoEncontrado.telefone);
+					 }else {
+						 System.out.println("	Contato inexistente ou nome errado.");
+					 }
 			 
-			 System.out.println("\n	(Continuar buscando? (S/N)");
-			 String continuar = ler.next();
+					 System.out.println("\n	(Continuar buscando? (S/N)");
+					 String continuar = ler.next();
 			 
-			 if (continuar.equalsIgnoreCase("N")) {
+					 if (continuar.equalsIgnoreCase("N")) {
+						 break;
+					 }
+				 }
 				 break;
-			 }
+
+			 case 3:
+				  while( true ) {
+					  	System.out.println("    Remover Contatos: \n");
 			 
-		 }
+					  		System.out.println("Nome: ");
+					  		String nomeDoContato = ler.next();
+			 
+			
+					  		Contato contatoEncontrado = lista_sequencial.removerContato(nomeDoContato);
+
+			 
+					  		
+					  		System.out.println("\n	(Continuar removendo? (S/N)");
+					  		String continuar = ler.next();
+			 
+					  		if (continuar.equalsIgnoreCase("N")) {
+					  			break;
+					  		}
+				  }
+				break;
+			 
+				case 4:
+					System.out.println("Exibindo Lista:\n");
+					
+					 lista_sequencial.ordenarLista();
+					 lista_sequencial.exibirLista();
+					break;
+					
+				case 5: 
+					System.out.println("Saindo do Programa...");
+					break;
+				  } 
+			 break;
 		 
-		 while( true ) {
-			 System.out.println("    Remover Contatos: \n");
 			 
-			 System.out.println("Nome: ");
-			 String nomeDoContato = ler.next();
+		  }
 			 
-			
-			 Contato contatoEncontrado = lista_sequencial.removerContato(nomeDoContato);
-
 			 
-			 lista_sequencial.exibirLista();
-			 System.out.println("\n	(Continuar removendo? (S/N)");
-			 String continuar = ler.next();
-			 
-			 if (continuar.equalsIgnoreCase("N")) {
-				 break;
-			 }
 			 
 			
+		 
+		 
+		 
+		 
 		
-	} 
+			
+		
+			} 
 
-		
-			 
-	}
+					 
 }
